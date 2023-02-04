1dada7fb8be48c286058c3b1ac936af4edd70c0d
Documenter.jl <documenter@juliadocs.github.io>
2023-02-04 19:32:09 +0000

    build based on 870e86c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = MRSI","category":"page"},{"location":"#MRSI","page":"Home","title":"MRSI","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MRSI.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [MRSI]","category":"page"}]
+}
