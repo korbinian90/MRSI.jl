cb0e6899a85165eba73ad50eca5adf7f06124ff4
korbinian90 <korbinian90@gmail.com>
2023-04-18 11:11:06 +0200

    fix coil_combine bug

--- src/coil_combination.jl
+++ src/coil_combination.jl
@@ -1,17 +1,19 @@
 function coil_combine(data, refscan)
     coil_weight = conj(refscan[:, :, :, [1], :]) # refscan - only use one timepoint and one dim6
-    combined = zeros(eltype(data), size(data)[1:4])
-    for cha in axes(data, 5)
-        combined .+= data[:, :, :, :, cha] .* coil_weight[:, :, :, :, cha]
-    end
-    combined .*= scaling(coil_weight)
-
-    combined = dropdims(combined; dims=5)
-    return combined
+    combined = combine(data, coil_weight)
+    return combined .* scaling(coil_weight)
 end
 
 function scaling(coil_weight)
     scale = 1 ./ sum(abs.(coil_weight) .^ 2; dims=5)
     scale[.!isfinite.(scale)] .= 0
     return scale
+end
+
+function combine(data, weight)
+    combined = zeros(eltype(data), size(data)[1:4])
+    for cha in axes(data, 5)
+        combined .+= data[:, :, :, :, cha] .* weight[:, :, :, :, cha]
+    end
+    return combined
 end
