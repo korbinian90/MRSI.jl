84dcb3f5d79e828fd201f54e3674f464dbf0932f
Korbinian Eckstein <korbinian90@gmail.com>
2024-05-28 13:17:11 +0200

    lipid suppression refactor; add berni masking

--- src/lipid_decon/lipid_mask.jl
+++ src/lipid_decon/lipid_mask.jl
@@ -1,10 +1,48 @@
+# Idea to improve: take maximum instead of sum for fat
 function lipid_mask_from_mrsi(data, info; fat_range_ppm=[1.8, 0.5], threshold=0.1)
     dims = 4
-    spectrum = reverse(fftshift(fft(data, dims), dims));
+    spectrum = reverse(fftshift(fft(data, dims), dims))
     fat_range = ppm_to_vecsize_point.(Ref(info), fat_range_ppm)
     fat = sum(abs.(selectdim(spectrum, dims, fat_range[1]:fat_range[2])); dims)
     all = sum(abs.(spectrum); dims)
     return (fat ./ all) .> threshold
+end
+
+# Problem: requires brain mask
+function lipid_mask_from_mrsi_pipeline(csi, brain_mask; fat_range_ppm=[1.8, 0.5], threshold=0.1)
+    dims = 4
+    spectrum = fftshift(fft(csi, dims), dims)
+
+    f_range = size(csi, 4)
+    f_range_start = ceil(f_range * 0.82)
+    f_range_end = ceil(f_range * 0.96)
+
+    for I in eachindex(spectrum)
+        ftspectramax[I] = max(spectrum[I, f_range_start:f_range_end])
+    end
+
+    lipid_mask = ftspectramax .>= 0.3 * maximum(ftspectramax)
+
+    tumor_mask = imerode(brain_mask, strel_disk(1))
+    lipid_mask = lipid_mask .- tumor_mask .= 1
+
+    corner_mask = .!imdilate(brain_mask, strel_disk(6))
+    lipid_mask = lipid_mask .- corner_mask .= 1
+
+    lipid_mask_total = lipid_mask_total + lipid_mask
+    return lipid_mask_total
+end
+
+function strel_disk(r)
+    struct_elm = ones(Int, 2 * r + 1, 2 * r + 1)
+    for i in 1:(2*r+1)
+        for j in 1:(2*r+1)
+            if ((i - r - 1)^2 + (j - r - 1)^2) > r^2
+                struct_elm[i, j] = 0
+            end
+        end
+    end
+    return struct_elm
 end
 
 function ppm_to_vecsize_point(info, ppm, center_around_ppm=4.65)
--- src/lipid_decon/lipid_suppression_L2.jl
+++ src/lipid_decon/lipid_suppression_L2.jl
@@ -1,38 +1,3 @@
-function lipid_suppression!(csi, mask, lipid_mask=trues(size(csi)[1:3]); type=:L2, L2_beta=0.1f0, L1_n_loops=5, args...)
-    for slice in axes(csi, 3)
-        csi[:, :, slice, :] = lipid_suppression_slice(csi[:, :, slice, :], mask[:, :, slice], lipid_mask[:, :, slice], type, L2_beta, L1_n_loops, args...)
-    end
-end
-
-function lipid_suppression_slice(csi, mask, lipid_mask, type, L2_beta, L1_n_loops, args...)
-    fft_dim = 3 # spectrum
-    csi = fftshift(fft(csi, fft_dim), fft_dim)
-
-    lipid_basis = generate_lipid_basis(csi, lipid_mask) # Q: kann man hier auch gleich lipid basis definieren, anstatt die lipid_basis aus der lipid_mask zu generieren? Die lipid_mask wird ja dann auch aus dem Spektrum generiert
-    if type == :L2
-        csi = lipid_decontamination_L2(csi, lipid_basis, mask, L2_beta) # brain_mask looks unimportant (only for efficiency?)
-    elseif type == :L1
-        csi = lipid_decontamination_L1(csi, lipid_basis, mask, L1_n_loops; args...)
-    end
-
-    csi = ifft(ifftshift(csi, fft_dim), fft_dim)
-    return csi
-end
-
-function generate_lipid_basis(csi, lipid_mask)
-    count = 0
-    lipid_basis = zeros(eltype(csi), sum(lipid_mask), size(csi, 3))
-
-    for J in CartesianIndices(lipid_mask)
-        if lipid_mask[J]
-            count += 1
-            lipid_basis[count, :] = csi[J, :]
-        end
-    end
-
-    return lipid_basis
-end
-
 function lipid_decontamination_L2(csi, lipid_basis, mask, beta)
     beta *= 1.0f-15
     lipid_basis = Matrix(transpose(lipid_basis))
